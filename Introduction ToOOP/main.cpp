08d3b821a52ab3c23bf8c1f7243f0189a95554ad
Андрей Полищук <dcnfandrey@mail.ru>
2025-06-27 20:07:15 +0300

    Add project files.

---
+++ Introduction ToOOP/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+class Point
+{
+	double x;
+	double y;
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a; //Объявление переменной 'а' типа 'int'
+	Point A; //Объявление переменной 'A' типа 'Point'
+			//Создание объекта 'А' структуры 'Point'
+			//Создание экземпляра 'А' структуры 'Point'
+			//'A' is instance of struct 'Point'
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+
+	Point* pA = &A;
+
+	cout << pA->x << "\t" << pA->y << endl;
+
+
+}
