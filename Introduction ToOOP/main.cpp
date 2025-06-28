1cd829673b4727e0de761ed0c7404699edc98e5f
Андрей Полищук <dcnfandrey@mail.ru>
2025-06-28 10:02:08 +0300

    distance method and distance function DONE

--- Introduction ToOOP/main.cpp
+++ Introduction ToOOP/main.cpp
@@ -22,7 +22,20 @@ public:
 	{
 		this->y = y;
 	}
+	double distance(Point other)
+	{
+		double x_diff = this->get_x() - other.get_x();
+		double y_diff = this->get_y() - other.get_y();
+		return sqrt(x_diff * x_diff + y_diff * y_diff);
+	}
 };
+
+double distance(Point *pA, Point *pB)
+{
+	double x_diff = pA->get_x() - pB->get_x();
+	double y_diff = pA->get_y() - pB->get_y();
+	return sqrt(x_diff * x_diff + y_diff * y_diff);
+}
 
 //#define STRUCT_POINT
 void main()
@@ -48,4 +61,23 @@ void main()
 	A.set_x(2);
 	A.set_y(3);
 	cout << A.get_x() << "\t" << A.get_y() << endl;
+
+	Point B;
+	B.set_x(5);
+	B.set_y(7);
+	Point* pA = &A;
+	Point* pB = &B;
+	
+	Point C;
+	double x, y;
+	cout << "Введите координаты точки, до которой надо вычислить расстояние: "; cin >> x; cin >> y;
+	C.set_x(x); C.set_y(y);
+	cout << "Расстояние между точками A и B: " << distance(pA, pB) << endl;
+	cout << "Расстояние между точками B и A: " << distance(pB, pA) << endl;
+	cout << "Расстояние между точками A и B: " << A.distance(B) << endl;
+	cout << "Расстояние между точками A и C: " << A.distance(C) << endl;
+	cout << "Расстояние между точками B и C: " << B.distance(C) << endl;
+	cout << "Расстояние между точками B и A: " << B.distance(A) << endl;
+	main();
+
 }
