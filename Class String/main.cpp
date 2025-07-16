d7c5dfc9737ac73e7a32b9522422115203a99dbe
Андрей Полищук <dcnfandrey@mail.ru>
2025-07-16 19:52:34 +0300

    Subscript operator (index operator) in String DONE

--- Class String/main.cpp
+++ Class String/main.cpp
@@ -58,13 +58,26 @@ public:
 	//				Operators
 	String& operator=(const String& other)
 	{
+		//0) Проверяется, не является ли тот объект этим объектом
 		if (this == &other) return *this;
+		//1) Удаляем старую динамическую память
 		delete[] this->str;
+		//2) Deep copy - Побитовое копирование
 		this->size = other.size;
+		//3) Выделяем новую динамическую память
 		this->str = new char[size] {};
 		for (int i = 0; i < size; i++) this->str[i] = other.str[i];
 		cout << "CopyAssignment:\t\t" << this << endl;
 		return *this;
+	}
+
+	char operator[](int i)const
+	{
+		return str[i];
+	}
+	char& operator[](int i)
+	{
+		return str[i];
 	}
 
 	//				Methods:
@@ -79,9 +92,11 @@ String operator+(const String& left, const String& right)
 {
 	String result(left.get_size() + right.get_size() - 1);
 	for (int i = 0; i < left.get_size(); i++)
-		result.get_str()[i] = left.get_str()[i];
+		result[i] = left[i];
+		//result.get_str()[i] = left.get_str()[i];
 	for (int i = 0; i < right.get_size(); i++)
-		result.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+		result[i + left.get_size() - 1] = right[i];
+		//result.get_str()[i + left.get_size() - 1] = right.get_str()[i];
 	return result;
 }
 
@@ -95,7 +110,8 @@ void Clear(char* str)
 	delete[] str;
 }
 
-//#define CONSTRUCTORS_CHECK
+#define CONSTRUCTORS_CHECK
+//#define COPY_SEMANTIC_CHECK
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -126,6 +142,7 @@ void main()
 	//delete[] arr;  
 #endif // CONSTRUCTORS_CHECK
 
+#ifdef COPY_SEMANTIC_CHECK
 	String str1 = "Hello";
 	cout << str1 << endl;
 
@@ -135,6 +152,8 @@ void main()
 	String str3 = "World";
 	str2 = str3;
 	cout << str2 << endl;
+#endif // COPY_SEMANTIC_CHECK
+
 
 
 }
